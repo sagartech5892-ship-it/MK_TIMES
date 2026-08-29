c1c4fe1558265058b5844f539d2d43073c350e85
sagartech5892-ship-it <sagartech5892@gmail.com>
2026-08-30 01:54:16 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,60 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged, updatePassword } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
-import { getFirestore, doc, getDoc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const siteRef = doc(db, "site", "main");
-export { signInWithEmailAndPassword, signOut, onAuthStateChanged, updatePassword, getDoc, setDoc, onSnapshot };
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  updatePassword
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  onSnapshot,
+  runTransaction
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+
+
+import {
+  firebaseConfig
+} from "./firebase-config.js";
+
+
+const app =
+  initializeApp(firebaseConfig);
+
+
+export const auth =
+  getAuth(app);
+
+
+export const db =
+  getFirestore(app);
+
+
+export const siteRef =
+  doc(
+    db,
+    "site",
+    "main"
+  );
+
+
+export {
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  updatePassword,
+  getDoc,
+  setDoc,
+  onSnapshot,
+  doc,
+  runTransaction
+};
